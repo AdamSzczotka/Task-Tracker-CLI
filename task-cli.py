62c5457341286ef4394c6b90a0a4195938c63dca
Adam Szczotka <114624941+AdamSzczotka@users.noreply.github.com>
2025-02-04 13:22:30 +0100

    feat: create Task class

---
+++ task-cli.py
@@ -0,0 +1,19 @@
+# Autor: Adam Szczotka
+# Title: Task Tracker CLI
+
+
+class Task:
+    def __init__(self, id, description, status, createdAt, updatedAt):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
